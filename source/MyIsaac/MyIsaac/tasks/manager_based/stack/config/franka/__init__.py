e0e2eded2a47287d37dcf659d2b99d6f96766356
Notebook <grape43146@naver.com>
2025-05-09 12:31:30 +0900

    NoteBook :

--- source/MyIsaac/MyIsaac/tasks/manager_based/stack/config/franka/__init__.py
+++ source/MyIsaac/MyIsaac/tasks/manager_based/stack/config/franka/__init__.py
@@ -51,6 +51,9 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": stack_ik_rel_env_cfg.FrankaCubeStackEnvCfg,
         "robomimic_bc_cfg_entry_point": os.path.join(agents.__path__[0], "robomimic/bc_rnn_low_dim.json"),
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:FrankaReachPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
     disable_env_checker=True,
 )
