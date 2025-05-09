c65f417914844df685255d440714d95cfbffd368
yoon <grape4314@cau.ac.kr>
2025-05-10 01:49:56 +0900

    Home : Add Orientation Rewards

--- source/MyIsaac/MyIsaac/tasks/manager_based/lift/mdp/rewards.py
+++ source/MyIsaac/MyIsaac/tasks/manager_based/lift/mdp/rewards.py
@@ -11,7 +11,7 @@ from typing import TYPE_CHECKING
 from isaaclab.assets import RigidObject
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.sensors import FrameTransformer
-from isaaclab.utils.math import combine_frame_transforms
+from isaaclab.utils.math import combine_frame_transforms, quat_error_magnitude, quat_mul
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
@@ -65,3 +65,26 @@ def object_goal_distance(
     distance = torch.norm(des_pos_w - object.data.root_pos_w[:, :3], dim=1)
     # rewarded if the object is lifted above the threshold
     return (object.data.root_pos_w[:, 2] > minimal_height) * (1 - torch.tanh(distance / std))
+
+
+def object_goal_orientation(
+        env: ManagerBasedRLEnv,
+        minimal_height: float,
+        command_name: str,
+        robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+        object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+) -> torch.Tensor:
+    """Penalize tracking orientation error using shortest path.
+    The function computes the orientation error between the desired orientation (from the command) and the
+    current orientation of the asset's body (in world frame). The orientation error is computed as the shortest
+    path between the desired and current orientations.
+    """
+    # extract the asset (to enable type hinting)
+    robot: RigidObject = env.scene[robot_cfg.name]
+    object: RigidObject = env.scene[object_cfg.name]
+    command = env.command_manager.get_command(command_name)
+    # obtain the desired and current orientations
+    des_quat_b = command[:, 3:7]
+    des_quat_w = quat_mul(object.data.root_state_w[:, 3:7], des_quat_b)
+    curr_quat_w = object.data.body_state_w[:, object_cfg.body_ids[0], 3:7]  # type: ignore
+    return (object.data.root_pos_w[:, 2] > minimal_height) * quat_error_magnitude(curr_quat_w, des_quat_w)
